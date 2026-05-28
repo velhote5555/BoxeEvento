b764248e751ee4fb397533faf590ebf5769b3ba6
velhote5555 <104162228+velhote5555@users.noreply.github.com>
2026-05-28 12:46:58 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/titasbet-site/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
